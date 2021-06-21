efdd78c4821c3ea1f8dd8796cf98b411fc39a38b
L.R. McFarland <lrm@starbug.com>
2021-06-21 10:49:16 -0700

    incomplete persistent storage

--- terraform/EBS_volume.tf
+++ terraform/EBS_volume.tf
@@ -2,12 +2,10 @@
 
 # warning: /dev/xvdh is not designed to persist. best use case: partition for logs
 
-# TODO store already created and has snapshot_id and not destroyed
-# TODO snapshot_id = "snap-073cb7ab26e8050be" pre created
-# TODO encrypted = true?
-
+# /dev/sdh
 
 # created, formated and destroyed with instance
+
 resource "aws_ebs_volume" "alpfs_devsdh" {
 
   availability_zone = aws_instance.alpfs.availability_zone
@@ -25,8 +23,11 @@ resource "aws_volume_attachment" "attach_devsdh" {
   instance_id = aws_instance.alpfs.id
 
   force_detach = true
+
   # TODO skip_destroy = true
   # TODO delete_on_termination = false
+  # TODO encrypted = true?
+
 }
 
 resource "null_resource" "MountDevSdh"  {
@@ -53,3 +54,53 @@ resource "null_resource" "MountDevSdh"  {
   }
 
 }
+
+# /dev/sdf
+
+# This idea is to have existing persistent storage and attach to it, but
+# this fails if the new instance is not in the same availabilty zone as the storage
+# but this has no contorl over that yet. and a copy in each zone seems excessive.
+
+# TODO create separate task to attch after zone is known?
+
+# persistent storage of an existing volume created with web ui atm. todo with terraform. conditionals?
+# needs volume id from envvar
+
+
+# resource "aws_volume_attachment" "alpfs_persistore" {
+#   device_name  = "/dev/sdf"
+#   volume_id    = var.ebs_volume_id
+#   instance_id  = aws_instance.alpfs.id
+#   force_detach = true
+# 
+#   # TODO availablity zone? multiple creations in each zone?
+#   # availability_zone = aws_instance.alpfs.availability_zone
+# 
+# }
+# 
+# 
+# resource "null_resource" "MountALPFSPersistore"  {
+# 
+#   depends_on = [
+#     aws_volume_attachment.alpfs_persistore,
+#   ]
+# 
+#   connection {
+#     type     = "ssh"
+#     user     = var.ami_user
+#     private_key = tls_private_key.createkey.private_key_pem
+#     host     = aws_instance.alpfs.public_ip
+#   }
+# 
+#   provisioner "remote-exec" {
+#     inline = [
+#       # todo via terraform, first time only "sudo mkfs -t xfs /dev/xvdf",
+#       "sudo mkdir /mnt/sdf",
+#       "sudo chown ubuntu:ubuntu /mnt/sdf",
+#       "sudo mount /dev/xvdf /mnt/sdf",
+#       "cd /mnt/sdf && ls",
+#     ]
+#   }
+# 
+# }
+#
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -23,7 +23,6 @@ variable "ami_user" {
   default     = "ubuntu"
 }
 
-
 variable "aws_access_key" {
   description = "AWS access key"
   type        = string
@@ -47,5 +46,10 @@ variable "docker_user" {
 # # TODO WARNING! Your password will be stored unencrypted in /home/ubuntu/snap/docker/
 variable "docker_access_token" {
   description = "docker access token"
+  type        = string
+}
+
+variable "ebs_volume_id" {
+  description = "EBS volume id"
   type        = string
 }
